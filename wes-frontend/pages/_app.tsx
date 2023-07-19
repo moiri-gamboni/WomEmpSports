4178d44ecee7d58c455596e975d385fea81f3880
Moiri Gamboni <moiri.gamboni@proton.me>
2023-07-19 10:48:10 +0200

    Set up ChakraProvider

--- wes-frontend/pages/_app.tsx
+++ wes-frontend/pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp

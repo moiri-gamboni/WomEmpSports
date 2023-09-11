42596e6f25f0e0ab57a55e1aae92286ec899fed7
Moiri Gamboni <moiri.gamboni@proton.me>
2023-09-11 18:37:53 +0200

    Remove unecessary Props interface

--- client/components/fixed-width-container.tsx
+++ client/components/fixed-width-container.tsx
@@ -1,13 +1,6 @@
 import { Box, BoxProps } from '@chakra-ui/react'
 
-export interface FixedWidthContainerProps extends BoxProps {
-  children: React.ReactNode
-}
-
-export default function FixedWidthContainer({
-  children,
-  ...props
-}: FixedWidthContainerProps) {
+export default function FixedWidthContainer({ children, ...props }: BoxProps) {
   return (
     <Box maxW='80rem' w='full' px={6} {...props}>
       {children}

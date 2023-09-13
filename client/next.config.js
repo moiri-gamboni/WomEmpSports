a4159fee0717110c9e9ae4ea4d2747cb737784cc
Moiri Gamboni <moiri.gamboni@proton.me>
2023-09-13 18:28:52 +0200

    Update nextjs image config to use remotePatterns

--- client/next.config.js
+++ client/next.config.js
@@ -1,21 +1,19 @@
-if (!process.env.WORDPRESS_API_URL) {
+if (!process.env.WORDPRESS_URL) {
   throw new Error(`
     Please provide a valid WordPress instance URL.
-    Add to your environment variables WORDPRESS_API_URL.
+    Add to your environment variables WORDPRESS_URL.
   `)
 }
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    // TODO: replace domains with remotePatterns
-    // https://nextjs.org/docs/pages/api-reference/components/image#remotepatterns
-    domains: [
-      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
-      '0.gravatar.com',
-      '1.gravatar.com',
-      '2.gravatar.com',
-      'secure.gravatar.com',
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: process.env.WORDPRESS_URL,
+        pathname: '/wp-content/uploads/**',
+      },
     ],
   },
   i18n: {
